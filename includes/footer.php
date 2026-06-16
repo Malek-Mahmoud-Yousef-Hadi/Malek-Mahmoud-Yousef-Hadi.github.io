e22e3ee142f5fad610c08e1e4845533011316406
Malek Mahmoud Yousef Hadi <2200906115@st.aabu.edu.jo>
2026-06-16 20:57:11 +0300

    my project

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+    <footer class="ghost-footer">
+        <p>SYSTEM_STATUS: OPERATIONAL | OPERATOR: MALEK_HADI | &copy; <?php echo date("Y"); ?></p>
+    </footer>
+    <script type="text/babel" src="assets/js/roadmap.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
